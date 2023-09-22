4764955c18ac2d866345a67d3590861968b21671
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-09-22 17:24:54 -0300

    Update

--- js/index.js
+++ js/index.js
@@ -5,32 +5,32 @@ let scrollBtn = document.getElementById('scrollBtn');
 let navMobile = document.querySelector('.navbar');
 let mobileAnchors = document.querySelector('.MobileAnchors');
 
-menuB.addEventListener('click', ()=>{
-    if(menuB.checked){
+menuB.addEventListener('click', () => {
+    if (menuB.checked) {
         navMobile.classList.add("mobileNav");
         mobileAnchors.style.display = 'block'
-    }else{
+    } else {
         navMobile.classList.remove("mobileNav");
         mobileAnchors.style.display = 'none';
     }
 })
-scrollBtn.addEventListener('click', ()=> {
+scrollBtn.addEventListener('click', () => {
     let screenWidth = window.innerWidth;
     let intro = document.getElementById('intro');
     let intro2 = document.getElementById('intro2');
-    if(screenWidth<800){
+    if (screenWidth < 800) {
         console.log('mobile scroll')
-        intro2.scrollIntoView({ behavior: "smooth"});
-    }else{
+        intro2.scrollIntoView({ behavior: "smooth" });
+    } else {
         console.log('pc scroll')
-        intro.scrollIntoView({ behavior: "smooth"});
+        intro.scrollIntoView({ behavior: "smooth" });
     }
-  });
+});
 for (const btn of scrollTop) {
-    btn.addEventListener("click", function() {
+    btn.addEventListener("click", function () {
         windowTop.scrollIntoView({ behavior: "smooth" });
         navMobile.classList.remove("mobileNav");
         mobileAnchors.style.display = 'none';
-        
+        menuB.checked = false;
     });
-  }
+}
