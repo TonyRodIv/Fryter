d7fa190f561949550f3bb273d21f68aca5d2cfd9
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-09-22 08:59:16 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-const teste = 0;
+let menuB = document.getElementById('burger');
+let navMobile = document.querySelector('.navbar');
+let mobileAnchors = document.querySelector('.MobileAnchors');
+
+menuB.addEventListener('click', ()=>{
+    if(menuB.checked){
+        navMobile.classList.add("mobileNav");
+        mobileAnchors.style.display = 'block'
+    }else{
+        navMobile.classList.remove("mobileNav")
+        mobileAnchors.style.display = 'none'
+    }
+})
