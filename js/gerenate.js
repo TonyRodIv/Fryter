8d5cf3a36df1723625ad24ea8a22c035d55027be
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-09-27 11:16:57 -0300

    Add files via upload

--- js/gerenate.js
+++ js/gerenate.js
@@ -1,4 +1,49 @@
 const mainView = document.getElementById('mainView');
+const checkPageHome = document.getElementById('checkPageHome');
+const checkPageTeam = document.getElementById('checkPageTeam');
+const checkPageDoc = document.getElementById('checkPageDoc');
+let pageAnchorCheck = document.querySelectorAll('.pageAnchorCheck')
+
+window.addEventListener('load', () => {
+    checkPageHome.checked = true
+});
+
+// for (const pageAnchorCheckers of pageAnchorCheck) {
+//     pageAnchorCheckers.addEventListener('click', () => {
+//         if (checkPageHome.checked) {
+//             console.log('Home')
+//             checkPageDoc.checked = false
+//             checkPageTeam.checked = false
+//         } else if (checkPageDoc.checked) {
+//             console.log('Doc')
+//             checkPageHome.checked = false
+//             checkPageTeam.checked = false
+//         } else if (checkPageTeam.checked) {
+//             console.log('Team')
+//             checkPageDoc.checked = false
+//             checkPageHome.checked = false
+//         }
+//     })
+// }
+
+checkPageHome.addEventListener('click', () => {
+    console.log('Home')
+    checkPageDoc.checked = false
+    checkPageTeam.checked = false
+})
+checkPageTeam.addEventListener('click', () => {
+    console.log('Team')
+    checkPageDoc.checked = false
+    checkPageHome.checked = false
+})
+checkPageDoc.addEventListener('click', () => {
+    console.log('Doc')
+    checkPageHome.checked = false
+    checkPageTeam.checked = false
+})
+
+
+
 
 
 
